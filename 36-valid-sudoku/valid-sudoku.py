a7df305f118e2961477cc3f8036184964fe96e26
Abishek S R <131516082+Abishek0411@users.noreply.github.com>
2025-06-04 15:17:25 +0530

    Time: 4 ms (59.14%) | Memory: 17.7 MB (69.35%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,9 +1,13 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        res = []
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        res = set()
         for i in range(9):
             for j in range(9):
                 element = board[i][j]
                 if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
+                    if (i, element) in res or (element, j) in res or (i // 3, j // 3, element) in res :
+                        return False
+                    res.add((i, element))
+                    res.add((element, j))
+                    res.add((i // 3, j // 3, element))
+        return True
