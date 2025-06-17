8b84050a417d6f314b891180cb97bc3b022ff382
Abishek S R <131516082+Abishek0411@users.noreply.github.com>
2025-06-17 21:08:43 +0530

    Time: 3 ms (82.31%) | Memory: 17.7 MB (69.46%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,10 +4,10 @@ class Solution:
         for i in range(9):
             for j in range(9):
                 element = board[i][j]
-                if element != '.':
-                    if (i, element) in res or (element, j) in res or (i // 3, j // 3, element) in res :
+                if element != ".":
+                    if (i, element) in res or (element, j) in res or (i//3, j//3, element) in res:
                         return False
                     res.add((i, element))
                     res.add((element, j))
-                    res.add((i // 3, j // 3, element))
+                    res.add((i//3, j//3, element))
         return True
