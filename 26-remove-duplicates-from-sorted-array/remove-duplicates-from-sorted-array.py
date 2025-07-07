bbfea825c74038fa82e623b74b1c35426f1a803d
Abishek S R <131516082+Abishek0411@users.noreply.github.com>
2025-07-08 01:05:10 +0530

    Time: 1 ms (60.08%) | Memory: 19.1 MB (29.46%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -5,7 +5,7 @@ class Solution:
 
         l = 1
         for r in range(1, len(nums)):
-            if nums[r] != nums[r-1]:
+            if nums[r] != nums[r-1]:  # [l-1] actually
                 nums[l] = nums[r]
                 l+=1
         return l
